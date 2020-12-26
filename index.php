dcbfdac97219355c5b1f40b2b266fc9fa482ebaa
unknown <zxst01@mail.ru>
2020-12-26 20:12:42 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello git</title>
+</head>
+<body>
+    <h1>Hello git</h1>
+    <h2>Hello web developer</h2>
+<script src="script.js"></script>
+</body>
+</html>
